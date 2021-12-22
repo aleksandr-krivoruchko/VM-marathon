7bafd9d2bf9b5385ab10b07499b9f81c81fa7ff0
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-22 23:14:25 +0200

    Сделано

--- src/js/dr-n-dr.js
+++ src/js/dr-n-dr.js
@@ -1,25 +1,46 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
 
-function sliderPlugin(activeSlide = 2) {
-		const slides = document.querySelectorAll('.slide');
-		const container = document.querySelector('.container');
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
 
-		container.addEventListener('click', onShowSlide);
-
-		slides[activeSlide].classList.add('active');
-
-		function onShowSlide(e) {
-
-		removeClassActive();
-		e.target.classList.add("active")
-		}
-
-
-		function removeClassActive() {
-
-			slides.forEach((slide) => {
-		slide.classList.remove("active");
-		})
-		}
+for (const placeholder of placeholders) {
+	placeholder.addEventListener('dragover', dragover);
+		placeholder.addEventListener('dragenter', dragenter);
+	placeholder.addEventListener('dragleave', dragleave);
+	placeholder.addEventListener('drop', dragdrop);
 }
 
-sliderPlugin(1);
+function dragstart(e) {
+	e.target.classList.add('hold');
+	setTimeout(() => {
+		e.target.classList.add('hide');
+	}, 0);
+}
+
+function dragend(e) {
+	e.target.classList.remove('hold', 'hide');
+}
+
+
+function dragover(e) {
+	e.preventDefault();
+}
+
+function dragenter(e) {
+		e.target.classList.add('hovered');
+
+}
+
+function dragleave(e) {
+	e.target.classList.remove('hovered');
+
+}
+
+function dragdrop(e) {
+	e.target.append(item);
+			e.target.classList.remove('hovered');
+
+}
+
+
