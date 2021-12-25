dd961f45b7e743523408955edd12e57c1e6913e4
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-25 17:40:50 +0200

    Update

--- src/js/hover-board.js
+++ src/js/hover-board.js
@@ -4,25 +4,21 @@ const colors = ['#FF1493', '#FF6347', '#4B0082', '#9400D3', '#7FFF00', '#20B2AA'
 
 for (let i = 0; i < SQUARES_NUM; i += 1) {
 const square = document.createElement("div");
-square.addEventListener("mouseover", () => {setColor(square)});
-square.addEventListener("mouseleave", () => {removeColor(square)});
-
+square.addEventListener("mouseover", setColor);
+square.addEventListener("mouseleave", removeColor);
 square.classList.add("square")	;
 board.append(square);
 }
 
-function setColor(el) {
+function setColor(e) {
 	const color = getRandomColor();
-	el.style.backgroundColor = color;
-		el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+	e.target.style.backgroundColor = color;
+		e.target.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(el) {
-	el.style.backgroundColor = "#1d1d1d";
-	el.style.boxShadow = '0 0 2px #000';
+function removeColor(e) {
+	e.target.style.backgroundColor = "#1d1d1d";
+	e.target.style.boxShadow = '0 0 2px #000';
 }
 
-function getRandomColor() {
-	const index = Math.floor(Math.random() * colors.length);
-	return colors[index];
-}
+const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
